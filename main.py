e58da7d427d577acfb034a38409e2c9e7b1bac4d
Ushi Philip <ushi7670741@gmail.com>
2026-05-12 20:40:09 +0300

    created a file named main, with all the basic function using the pass method

---
+++ main.py
@@ -0,0 +1,45 @@
+import random
+
+def get_a_number():
+    pass
+
+
+def chacks_number(num):
+    pass
+
+
+def hide_word(len):
+    pass
+
+
+def get_a_letter():
+    pass
+
+
+def checks_alpha(letter):
+    pass
+
+
+def show_status(inputed_letters, hidden_word, guess_times):
+    pass
+
+
+def main():
+    guesses_times = get_a_number()
+
+    word_list = []
+
+    current_word = word_list[random.randint(0, len(word_list))]
+
+    hidden_word = hide_word(len(current_word))
+
+    inputed_letters = []
+
+    while guesses_times > 0:
+        pass
+
+    if guesses_times > 0:
+        pass
+
+if __name__ == "__main__":
+    main()
